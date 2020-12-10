7e6d83a8c79b20bc404cc33c0816eecdd04404d7
Noah Friedman <noahfriedman2@gmail.com>
2020-12-10 13:18:57 -0500

    Add example for Polynomial.F

--- polynomial_examples_test.go
+++ polynomial_examples_test.go
@@ -6,5 +6,13 @@ func ExamplePolynomial_Degree() {
 	p := Polynomial{5, -12, 4}
 	// 4x² - 12x + 5 --> highest exponent = 2
 
-	fmt.Println(p.Degree()) // Output: 2
+	fmt.Print(p.Degree()) // Output: 2
+}
+
+func ExamplePolynomial_F() {
+	x := 7.0
+	p := Polynomial{3, 5}
+	// 5x + 3 -> 5(7) + 3 == 38
+
+	fmt.Print(p.F(x)) // Output: 38
 }
