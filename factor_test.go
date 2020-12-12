8ddb6270793d975c5cb1f344a26162c69f9dab28
Noah Friedman <noahfriedman2@gmail.com>
2020-12-12 12:13:58 -0500

    Update tests for Factor function [skip ci]

--- factor_test.go
+++ factor_test.go
@@ -10,23 +10,33 @@ import (
 var _ = Describe("the Factor function", func() {
 	rand.Seed(GinkgoRandomSeed())
 
-	It("should correctly factor polynomial functions", func() {
-		By("using a simple polynomial function")
+	It("should factor a simple polynomial function", func() {
 		p := Polynomial{10, 7, 1}
+		f := Factor(p, 2)
 
-		f := Factor(&p)
-		Expect(*f.Unfactored).To(Equal(p))
+		Expect(f.Unfactored).To(Equal(p))
 
 		Expect(f.Raw).To(ContainSubstring("(x + 2)"))
 		Expect(f.Raw).To(ContainSubstring("(x + 5)"))
 
-		Expect(f.XIntercepts).To(ContainElement(-2))
-		Expect(f.XIntercepts).To(ContainElement(-5))
+		Expect(f.XIntercepts).To(HaveLen(2))
+		Expect(f.XIntercepts).To(ContainElements(-2, -5))
+	})
+	It("should factor a longer polynomial function", func() {
+		p := Polynomial{1, -4, 0, 7, 2}
+		f := Factor(p, 2)
 
-		Expect(f.Zeroes).To(ContainElement("x + 2"))
-		Expect(f.Zeroes).To(ContainElement("x + 5"))
+		Expect(f.Unfactored).To(Equal(p))
 
-		By("using a randomly generated polynomial function")
+		Expect(f.Raw).To(ContainSubstring("(x + 1)"))
+		Expect(f.Raw).To(ContainSubstring("(x + 0.50)"))
+		Expect(f.Raw).To(ContainSubstring("(x - 0.30)"))
+		Expect(f.Raw).To(ContainSubstring("(x + 3.30)"))
+
+		Expect(f.XIntercepts).To(HaveLen(4))
+		Expect(f.XIntercepts).To(ContainElements(-1, 0.5, 0.3, -3.3))
+	})
+	It("should factor a randomly generated simple polynomial function", func() {
 		var (
 			product, sum float64
 			products     = rand.Perm(50)
@@ -34,6 +44,9 @@ var _ = Describe("the Factor function", func() {
 		for _, p := range products {
 			if p == 1 {
 				continue
+			}
+			if rand.Intn(1) == 1 {
+				p *= -1
 			}
 
 			divisors := rand.Perm(p)
@@ -49,17 +62,15 @@ var _ = Describe("the Factor function", func() {
 				break
 			}
 		}
-		p = Polynomial{product, sum, 1}
-		f = Factor(&p)
+		p := Polynomial{product, sum, 1}
+		f := Factor(p, 2)
 
-		Expect(*f.Unfactored).To(Equal(p))
+		Expect(f.Unfactored).To(Equal(p))
 
 		Expect(f.XIntercepts).To(HaveLen(2))
 		Expect(f.XIntercepts[0] + f.XIntercepts[1]).To(Equal(sum))
 		Expect(f.XIntercepts[0] * f.XIntercepts[1]).To(Equal(product))
 		x := float64(rand.Intn(19) + 1)
 		Expect((x - f.XIntercepts[0]) * (x - f.XIntercepts[1])).To(Equal(p.F(x)))
-
-		Expect(f.Zeroes).To(HaveLen(2))
 	})
 })
