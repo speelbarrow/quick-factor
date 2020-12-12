4409d3afce33c0ea11e88859f8207e47bb9f5c17
Noah Friedman <noahfriedman2@gmail.com>
2020-12-12 13:44:30 -0500

    Update tests and implement some functionality for Factor function [skip ci]

--- factor.go
+++ factor.go
@@ -1,14 +1,108 @@
 // Define types and functions related to factoring.
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"sync"
+)
+
 // A polynomial in factored form.
 type FactoredPolynomial struct {
 	Raw         string
 	XIntercepts []float64
-	Zeroes      []string
-	Unfactored  *Polynomial
+	Unfactored  Polynomial
 }
 
-func Factor(unfactored *Polynomial) *FactoredPolynomial {
+// Factor a polynomial function. Round numbers to 'roundTo' decimal places.
+func Factor(unfactored Polynomial, roundTo int) *FactoredPolynomial {
+	r := &FactoredPolynomial{Unfactored: unfactored}
+
+	if unfactored.Degree() == 2 {
+		f := factorTrinomial(&unfactored)
+		switch f.(type) {
+		case *[2]float64:
+			for _, v := range *f.(*[2]float64) {
+				sign := "-"
+				if a := math.Abs(v); v != a {
+					sign = "+"
+				}
+
+				r.Raw += fmt.Sprintf(fmt.Sprintf("(x %s %%.%dg)", sign, roundTo), v)
+				r.XIntercepts = append(r.XIntercepts, v*-1)
+			}
+		case *[2]string:
+		}
+	}
+
+	return r
+}
+
+// A helper function that factors trinomials only. Panics if the provided Polynomial has a degree that is not equal to 2. Will return either *[2]float64 or *[2]string.
+func factorTrinomial(trinomial *Polynomial) interface{} {
+	if d := trinomial.Degree(); d != 2 {
+		log.Panicf("expected Polynomial with degree 2, got degree %d\n", d)
+	}
+
+	if (*trinomial)[0] != 0 {
+		if r := trinomialByGrouping(trinomial); r != nil {
+			return r
+		}
+	}
+
+	return trinomialByQuadraticFormula(trinomial)
+}
+
+// A helper function that factors trinomials by grouping. Returns nil if the trinomial cannot be factored by grouping.
+func trinomialByGrouping(trinomial *Polynomial) *[2]float64 {
+	var (
+		product = (*trinomial)[0] * (*trinomial)[2]
+		wg      sync.WaitGroup
+		group   = make(chan *[2]float64)
+		check   = func(a, b float64) *[2]float64 {
+			defer wg.Done()
+
+			if a*b == product {
+				if (*trinomial)[1] < 0 {
+					a *= -1
+					b *= -1
+				}
+
+				group <- &[2]float64{a, b}
+			}
+			return nil
+		}
+	)
+	defer close(group)
+
+	if product > 0 {
+		for a, b := math.Abs((*trinomial)[1])-1, 1.; a >= b; a, b = a-1, b+1 {
+			wg.Add(1)
+			go check(a, b)
+		}
+	} else if product < 0 {
+		for a, b := math.Abs((*trinomial)[1])+1, -1.; a*b <= product; a, b = a+1, b-1 {
+			wg.Add(1)
+			go check(a, b)
+		}
+	}
+
+	done := make(chan bool, 1)
+	go func() {
+		wg.Wait()
+		done <- true
+	}()
+
+	select {
+	case r := <-group:
+		return r
+	case <-done:
+		return nil
+	}
+}
+
+// A helper function that factors trinomials by using the quadratic formula.
+func trinomialByQuadraticFormula(trinomial *Polynomial) *[2]string {
 	return nil
 }
--- factor_test.go
+++ factor_test.go
@@ -16,11 +16,11 @@ var _ = Describe("the Factor function", func() {
 
 		Expect(f.Unfactored).To(Equal(p))
 
-		Expect(f.Raw).To(ContainSubstring("(x + 2)"))
-		Expect(f.Raw).To(ContainSubstring("(x + 5)"))
+		Expect(f.Raw).To(ContainSubstring("(x - 2)"))
+		Expect(f.Raw).To(ContainSubstring("(x - 5)"))
 
 		Expect(f.XIntercepts).To(HaveLen(2))
-		Expect(f.XIntercepts).To(ContainElements(-2, -5))
+		Expect(f.XIntercepts).To(ContainElements(-2., -5.))
 	})
 	It("should factor a longer polynomial function", func() {
 		p := Polynomial{1, -4, 0, 7, 2}
@@ -42,7 +42,7 @@ var _ = Describe("the Factor function", func() {
 			products     = rand.Perm(50)
 		)
 		for _, p := range products {
-			if p == 1 {
+			if p <= 1 {
 				continue
 			}
 			if rand.Intn(1) == 1 {
@@ -51,6 +51,10 @@ var _ = Describe("the Factor function", func() {
 
 			divisors := rand.Perm(p)
 			for _, divisor := range divisors {
+				if divisor == 0 {
+					continue
+				}
+
 				if d := float64(p) / float64(divisor); d == math.Round(d) {
 					product = float64(p)
 					sum = float64(divisor) + d
@@ -68,7 +72,7 @@ var _ = Describe("the Factor function", func() {
 		Expect(f.Unfactored).To(Equal(p))
 
 		Expect(f.XIntercepts).To(HaveLen(2))
-		Expect(f.XIntercepts[0] + f.XIntercepts[1]).To(Equal(sum))
+		Expect(math.Abs(f.XIntercepts[0] + f.XIntercepts[1])).To(Equal(sum))
 		Expect(f.XIntercepts[0] * f.XIntercepts[1]).To(Equal(product))
 		x := float64(rand.Intn(19) + 1)
 		Expect((x - f.XIntercepts[0]) * (x - f.XIntercepts[1])).To(Equal(p.F(x)))
