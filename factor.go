593ce931baec958f246f26933498d7ac34bcbe83
Noah Friedman <noahfriedman2@gmail.com>
2020-12-13 12:38:47 -0500

    Complete Factor function

--- factor.go
+++ factor.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"sort"
 	"sync"
 )
 
@@ -15,32 +16,53 @@ type FactoredPolynomial struct {
 	Unfactored  Polynomial
 }
 
-// Factor a polynomial function. Round numbers to 'roundTo' decimal places.
+// Factor a polynomial function. Round numbers displayed in the 'Raw' field to 'roundTo' decimal places. If the function is not factorable, the 'Raw' field will read "not factorable".
 func Factor(unfactored Polynomial, roundTo int) *FactoredPolynomial {
 	r := &FactoredPolynomial{Unfactored: unfactored}
 
-	if unfactored.Degree() == 2 {
-		f := factorTrinomial(&unfactored)
+	for unfactored.Degree() > 2 {
+		divisor := simplestFactor(&unfactored)
+		unfactored = *dividePolynomial(&unfactored, divisor)
+		r.Raw += formatFactoredForm(divisor, roundTo)
+		r.XIntercepts = append(r.XIntercepts, divisor)
+	}
+
+	if d := unfactored.Degree(); d == 2 {
+		f := factorTrinomial(&unfactored, roundTo)
 		switch f.(type) {
 		case *[2]float64:
 			for _, v := range *f.(*[2]float64) {
-				sign := "-"
-				if a := math.Abs(v); v != a {
-					sign = "+"
-				}
+				v *= -1
 
-				r.Raw += fmt.Sprintf(fmt.Sprintf("(x %s %%.%dg)", sign, roundTo), v)
-				r.XIntercepts = append(r.XIntercepts, v*-1)
+				r.Raw += formatFactoredForm(v, roundTo)
+				r.XIntercepts = append(r.XIntercepts, v)
 			}
 		case *[2]string:
+			r.Raw = fmt.Sprintf("(x - (%s))(x - (%s))", (*f.(*[2]string))[0], (*f.(*[2]string))[1])
 		}
+	} else if d == 1 {
+		x := (unfactored[0] * -1) / unfactored[1]
+		r.Raw = formatFactoredForm(x, roundTo)
+		r.XIntercepts = []float64{x}
+	} else { // In this case the degree must be equal to 0
+		r.Raw = "not factorable"
 	}
 
 	return r
 }
 
+// A helper function to transform a number into it's representation in factored form.
+func formatFactoredForm(x float64, roundTo int) string {
+	sign := "-"
+	if x != math.Abs(x) {
+		sign = "+"
+	}
+
+	return fmt.Sprintf(fmt.Sprintf("(x %s %%.%dg)", sign, roundTo), math.Abs(x))
+}
+
 // A helper function that factors trinomials only. Panics if the provided Polynomial has a degree that is not equal to 2. Will return either *[2]float64 or *[2]string.
-func factorTrinomial(trinomial *Polynomial) interface{} {
+func factorTrinomial(trinomial *Polynomial, roundTo int) interface{} {
 	if d := trinomial.Degree(); d != 2 {
 		log.Panicf("expected Polynomial with degree 2, got degree %d\n", d)
 	}
@@ -51,7 +73,7 @@ func factorTrinomial(trinomial *Polynomial) interface{} {
 		}
 	}
 
-	return trinomialByQuadraticFormula(trinomial)
+	return trinomialByQuadraticFormula(trinomial, roundTo)
 }
 
 // A helper function that factors trinomials by grouping. Returns nil if the trinomial cannot be factored by grouping.
@@ -82,7 +104,7 @@ func trinomialByGrouping(trinomial *Polynomial) *[2]float64 {
 			go check(a, b)
 		}
 	} else if product < 0 {
-		for a, b := math.Abs((*trinomial)[1])+1, -1.; a*b <= product; a, b = a+1, b-1 {
+		for a, b := math.Abs((*trinomial)[1])+1, -1.; a*b >= product; a, b = a+1, b-1 {
 			wg.Add(1)
 			go check(a, b)
 		}
@@ -102,7 +124,127 @@ func trinomialByGrouping(trinomial *Polynomial) *[2]float64 {
 	}
 }
 
-// A helper function that factors trinomials by using the quadratic formula.
-func trinomialByQuadraticFormula(trinomial *Polynomial) *[2]string {
-	return nil
+// A helper function that factors trinomials by using the quadratic formula. Will return either *[2]float64 or *[2]string.
+func trinomialByQuadraticFormula(trinomial *Polynomial, roundTo int) interface{} {
+	bSquaredMinus4ac := math.Pow((*trinomial)[1], 2) - (4 * (*trinomial)[2] * (*trinomial)[0])
+	if bSquaredMinus4ac < 0 {
+		firstHalf := fmt.Sprintf(fmt.Sprintf("(%%.%dg ", roundTo), (*trinomial)[1]*-1)
+		secondHalf := fmt.Sprintf(fmt.Sprintf(" √%%.%[1]dg) / %%.%[1]dg", roundTo), bSquaredMinus4ac, 2*(*trinomial)[2])
+
+		return &[2]string{
+			firstHalf + "+" + secondHalf,
+			firstHalf + "-" + secondHalf,
+		}
+	} else {
+		negativeB := (*trinomial)[1] * -1
+		squareRootOfBSquaredMinus4ac := math.Sqrt(bSquaredMinus4ac)
+		twoA := 2 * (*trinomial)[2]
+
+		// Multiply by -1 so that when it is flipped back in the Factor function it results in a valid answer.
+		return &[2]float64{
+			((negativeB + squareRootOfBSquaredMinus4ac) / twoA) * -1,
+			((negativeB - squareRootOfBSquaredMinus4ac) / twoA) * -1,
+		}
+	}
+}
+
+// A helper function to divide a polynomial with a degree > 2. Panics if division results in a remainder.
+func dividePolynomial(p *Polynomial, divisor float64) *Polynomial {
+	r := Polynomial{(*p)[len(*p)-1]}
+
+	for i, last := len(*p)-2, (*p)[len(*p)-1]*divisor; i > 0; i, last = i-1, r[0]*divisor {
+		r = append(Polynomial{(*p)[i] + last}, r...)
+	}
+
+	if rem := (*p)[0] + (r[0] * divisor); rem != 0 {
+		log.Panicf(
+			"invalid division - found remainder\n"+
+				"Original: %v\n"+
+				"Factored: %v\n"+
+				"Divisor: %g\n"+
+				"Remainder: %g\n",
+			*p, r, divisor, rem,
+		)
+	}
+
+	return &r
+}
+
+// A helper function to determine the simplest factor of a polynomial function with a degree < 2. Will return 0 if there is no factor.
+func simplestFactor(p *Polynomial) float64 {
+	var (
+		aFactors = findFactorsOfN((*p)[len(*p)-1])
+		cFactors = findFactorsOfN((*p)[0])
+		r        = make(chan float64, len(aFactors)*len(cFactors)) // Create buffered channel so it doesn't block when values are sent after first value (even though only the first value is read)
+		wg       sync.WaitGroup
+	)
+
+	for _, a := range aFactors {
+		for _, c := range cFactors {
+			wg.Add(1)
+			go func(a, c float64) {
+				defer wg.Done()
+
+				if p.F(c/a) == 0 {
+					r <- c / a
+				} else if p.F((c/a)*-1) == 0 {
+					r <- (c / a) * -1
+				}
+			}(a, c)
+		}
+	}
+
+	done := make(chan bool)
+	defer close(done)
+	go func() {
+		wg.Wait()
+		done <- true
+	}()
+
+	select {
+	case f := <-r:
+		<-done // Wait for all goroutines to finish before closing 'r' to avoid sending on a closed channel
+		close(r)
+		return f
+	case <-done:
+		return 0
+	}
+}
+
+// A helper function that finds all the factors of 'n'.
+func findFactorsOfN(n float64) []float64 {
+	r := []float64{1, n}
+
+	if n != math.Round(n) {
+		return r
+	}
+
+	var (
+		rLock sync.RWMutex
+		wg    sync.WaitGroup
+	)
+	for i := 2.; i < n; i++ {
+		wg.Add(1)
+		go func(i float64) {
+			defer wg.Done()
+
+			if d := n / i; d == math.Round(d) {
+				rLock.Lock()
+				r = append(r, d)
+				rLock.Unlock()
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	sort.Slice(r, func(i, j int) bool {
+		if r[i] > 0 && r[j] < 0 {
+			return true
+		} else if r[i] < 0 && r[j] > 0 {
+			return false
+		} else {
+			return r[i] < r[j]
+		}
+	})
+	return r
 }
--- factor_test.go
+++ factor_test.go
@@ -16,11 +16,23 @@ var _ = Describe("the Factor function", func() {
 
 		Expect(f.Unfactored).To(Equal(p))
 
-		Expect(f.Raw).To(ContainSubstring("(x - 2)"))
-		Expect(f.Raw).To(ContainSubstring("(x - 5)"))
+		Expect(f.Raw).To(ContainSubstring("(x + 2)"))
+		Expect(f.Raw).To(ContainSubstring("(x + 5)"))
 
 		Expect(f.XIntercepts).To(HaveLen(2))
 		Expect(f.XIntercepts).To(ContainElements(-2., -5.))
+	})
+	It("should be able to factor trinomial functions using the quadratic formula", func() {
+		p := Polynomial{-14, 5, 1}
+		f := Factor(p, 2)
+
+		Expect(f.Unfactored).To(Equal(p))
+
+		Expect(f.Raw).To(ContainSubstring("(x - 2)"))
+		Expect(f.Raw).To(ContainSubstring("(x + 7)"))
+
+		Expect(f.XIntercepts).To(HaveLen(2))
+		Expect(f.XIntercepts).To(ContainElements(2., -7.))
 	})
 	It("should factor a longer polynomial function", func() {
 		p := Polynomial{1, -4, 0, 7, 2}
@@ -29,12 +41,18 @@ var _ = Describe("the Factor function", func() {
 		Expect(f.Unfactored).To(Equal(p))
 
 		Expect(f.Raw).To(ContainSubstring("(x + 1)"))
-		Expect(f.Raw).To(ContainSubstring("(x + 0.50)"))
-		Expect(f.Raw).To(ContainSubstring("(x - 0.30)"))
-		Expect(f.Raw).To(ContainSubstring("(x + 3.30)"))
+		Expect(f.Raw).To(ContainSubstring("(x - 0.5)"))
+		Expect(f.Raw).To(ContainSubstring("(x - 0.3)"))
+		Expect(f.Raw).To(ContainSubstring("(x + 3.3)"))
 
 		Expect(f.XIntercepts).To(HaveLen(4))
-		Expect(f.XIntercepts).To(ContainElements(-1, 0.5, 0.3, -3.3))
+		By("rounding x intercepts to avoid incorrect test fails")
+
+		for i, v := range f.XIntercepts {
+			f.XIntercepts[i] = math.Round(v*10) / 10
+		}
+
+		Expect(f.XIntercepts).To(ContainElements(-1., 0.5, 0.3, -3.3))
 	})
 	It("should factor a randomly generated simple polynomial function", func() {
 		var (
@@ -76,5 +94,31 @@ var _ = Describe("the Factor function", func() {
 		Expect(f.XIntercepts[0] * f.XIntercepts[1]).To(Equal(product))
 		x := float64(rand.Intn(19) + 1)
 		Expect((x - f.XIntercepts[0]) * (x - f.XIntercepts[1])).To(Equal(p.F(x)))
+	})
+	It("should be able to factor binomial functions", func() {
+		p := Polynomial{-2, 3}
+		f := Factor(p, 2)
+
+		Expect(f.Unfactored).To(Equal(p))
+
+		Expect(f.Raw).To(Equal("(x - 0.67)"))
+
+		Expect(f.XIntercepts).To(Equal([]float64{2. / 3.}))
+	})
+	It("should not try to factor a monomial or empty Polynomial", func() {
+		By("using a monomial")
+		p := Polynomial{5}
+		f := Factor(p, 2)
+
+		Expect(f.Unfactored).To(Equal(p))
+		Expect(f.Raw).To(Equal("not factorable"))
+		Expect(f.XIntercepts).To(HaveLen(0))
+
+		By("using an empty Polynomial")
+		p = Polynomial{}
+		f = Factor(p, 2)
+
+		Expect(f.Raw).To(Equal("not factorable"))
+		Expect(f.XIntercepts).To(HaveLen(0))
 	})
 })
